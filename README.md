1fa19ee4c9628085c658dee7890558862125321a
Adrián Félix <guerofelix234@gmail.com>
2025-10-30 21:09:26 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WeatherNow
+iOS weather application developed with SwiftUI and MVVM architecture. Fetches and decodes JSON data from the OpenWeatherMap API, displays current conditions and forecasts, and demonstrates API integration best practices.
---
+++ WeatherNowTests/WeatherNowTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNowTests.swift
+//  WeatherNowTests
+//
+//  Created by Adrian Felix on 30/10/25.
+//
+
+import Testing
+@testable import WeatherNow
+
+struct WeatherNowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
